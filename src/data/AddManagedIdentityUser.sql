320022b1964d0fdf5c16d88a6203f7cab9cd23a5
Wik-dev <72396707+Wik-dev@users.noreply.github.com>
2025-02-17 17:56:02 +0000

    Add sql DB

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+CREATE USER id-hvpbkgcbkdki6 FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER id-hvpbkgcbkdki6;
+ALTER ROLE db_datawriter ADD MEMBER id-hvpbkgcbkdki6;
 
