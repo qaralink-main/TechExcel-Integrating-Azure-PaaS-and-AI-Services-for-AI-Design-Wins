63ce83e3ae7ceaefb4ab4abb6789bd2c0d1178d5
Wik-dev <72396707+Wik-dev@users.noreply.github.com>
2025-02-18 16:20:37 +0000

    Correct mapping api and managed indentity

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -79,6 +79,7 @@ app.MapGet("/", async () =>
 app.MapGet("/Hotels", async () => 
 {
     var hotels = await app.Services.GetRequiredService<IDatabaseService>().GetHotels();
+    return hotels;
 })
     .WithName("GetHotels")
     .WithOpenApi();
@@ -87,6 +88,7 @@ app.MapGet("/Hotels", async () =>
 app.MapGet("/Hotels/{hotelId}/Bookings/", async (int hotelId) => 
 {
     var bookings = await app.Services.GetRequiredService<IDatabaseService>().GetBookingsForHotel(hotelId);
+    return bookings;
 })
     .WithName("GetBookingsForHotel")
     .WithOpenApi();
@@ -95,6 +97,7 @@ app.MapGet("/Hotels/{hotelId}/Bookings/", async (int hotelId) =>
 app.MapGet("/Hotels/{hotelId}/Bookings/{min_date}", async (int hotelId, DateTime min_date) => 
 {
     var bookings = await app.Services.GetRequiredService<IDatabaseService>().GetBookingsByHotelAndMinimumDate(hotelId, min_date);
+    return bookings;
 })
     .WithName("GetRecentBookingsForHotel")
     .WithOpenApi();
--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER id-hvpbkgcbkdki6 FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER id-hvpbkgcbkdki6;
-ALTER ROLE db_datawriter ADD MEMBER id-hvpbkgcbkdki6;
+CREATE USER [id-hvpbkgcbkdki6] FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER [id-hvpbkgcbkdki6];
+ALTER ROLE db_datawriter ADD MEMBER [id-hvpbkgcbkdki6];
 
